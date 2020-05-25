1d7ef4779bb04063768ec831e3887d80a7e45696
caozheng <2435368142@qq.com>
2020-05-25 19:47:49 +0800

    远程测试调用项目

---
+++ http-demo/src/test/java/com/leyou/httpdemo/HttpDemoApplicationTest.java
@@ -0,0 +1,27 @@
+package com.leyou.httpdemo;
+
+import com.leyou.httpdemo.pojo.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HttpDemoApplication.class)
+public class HttpDemoApplicationTest {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Test
+    public void httpGet() {
+
+        User user = restTemplate.getForObject("http://localhost:8080/user/14", User.class);
+
+        System.out.println("user = " + user);
+
+    }
+
+}
